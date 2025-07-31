bb1a294e14d454efd20919ec414e187520098ca9
Ashraful <ashrafulrobin3@gmail.com>
2025-07-31 11:12:38 +0600

    Ignored symbolic links

--- sync.sh
+++ sync.sh
@@ -14,6 +14,6 @@ done
 
 while inotifywait -r -e modify,create,delete,move "$SOURCE"; do
   echo "Change detected. Syncing..."
-  rsync -avh --delete --no-owner --no-group --exclude-from="/data/exclude_patterns.txt" "$SOURCE/" "$DEST/"
+  rsync -avh --delete --no-owner --no-group --copy-links --no-links --exclude-from="/data/exclude_patterns.txt" "$SOURCE/" "$DEST/"
   echo "Sync done at $(date)"
 done
